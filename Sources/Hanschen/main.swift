e3d7ef15b4e578358b3e22fb4bfebad7723c8af9
Jacob Tyrrell <jacobtyrrell@icloud.com>
2020-05-30 17:34:12 +0100

    set up webhooks for support server

--- Sources/Hanschen/main.swift
+++ Sources/Hanschen/main.swift
@@ -3,27 +3,32 @@ import Foundation
 
 let options = ShieldOptions(prefixes: ["@bot", "h!"], willBeCaseSensitive: false, willDefaultHelp: true, willIgnoreBots: true)
 
-// Token options: devToken for Hanschen Edge and proToken for Hanschen (Production)
 let client = Shield(token: Config().token!, shieldOptions: options)
 
-//events
+// Events:
 client.addReadyEvent()
 client.addMessageCreateEvent()
 client.addGuildCreateEvent()
 client.addGuildDeleteEvent()
 
-//commands
+// Commands:
+// > General
 client.addInfoCmd()
 client.addPingCmd()
+client.addShardInfoCmd()
+client.addInviteCmd()
+
+// > Moderation
 client.addBanCmd()
 client.addKickCmd()
 client.addMuteCmd()
 client.addPruneCmd()
-client.addShardInfoCmd()
-//client.addPardonCmd()
-client.addInviteCmd()
 client.addUnmuteCmd()
 client.addUnbanCmd()
+
+// > Misc
+
+// > NSFW
 
 client.connect()
 
